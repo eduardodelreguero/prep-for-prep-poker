a7edf606a409fb425ba2b80cfa66906cb24e2e22
twant <taylor.want@gmail.com>
2017-07-13 22:03:53 -0500

    change "king_of diamonds" to "king_of_diamonds"
    ...otherwise the image file won't link.

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var cards = [ // an array_of_cards in square brackets
 'ace_of_spades', 'ace_of_clubs', 'ace_of_hearts', 'ace_of_diamonds',
-'king_of_spades', 'king_of_clubs', 'king_of_hearts', 'king_of diamonds',
+'king_of_spades', 'king_of_clubs', 'king_of_hearts', 'king_of_diamonds',
 'queen_of_spades', 'queen_of_clubs', 'queen_of_hearts', 'queen_of_diamonds',
 'jack_of_spades', 'jack_of_clubs', 'jack_of_hearts', 'jack_of_diamonds',
 '10_of_spades', '10_of_clubs', '10_of_hearts', '10_of_diamonds',
